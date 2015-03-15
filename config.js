34bdb814416a34a20915a0e43d36864244f19ff5
Rob Wormald <rob@jiff.com>
2015-03-14 23:44:29 -0700

    update config

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 System.config({
-  "baseURL": "/",
   "paths": {
-    "*": "src/*.js",
+    "app/*": "src/*.js",
     "github:*": "jspm_packages/github/*.js"
   }
 });
 
 System.config({
   "map": {
+    "angular2": "github:angular/angular@master/dist/js/prod/es5/angular2",
     "automattic/socket.io-client": "github:automattic/socket.io-client@1.3.5"
   }
 });
