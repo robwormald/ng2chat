82f8353c9d42a013c8bb8871039855540d747dca
Rob Wormald <rob@jiff.com>
2015-03-15 13:15:33 -0700

    es6++ configgy

--- config.js
+++ config.js
@@ -1,12 +1,18 @@
 System.config({
   "paths": {
     "app/*": "src/*.js",
-    "github:*": "jspm_packages/github/*.js"
+    "github:*": "jspm_packages/github/*.js",
+    "*": "*.js"
+  },
+  "traceurOptions": {
+    "annotations": true,
+    "types": true
   }
 });
 
 System.config({
   "map": {
+    "angular/zone.js": "github:angular/zone.js@0.4.1",
     "angular2": "github:angular/angular@master/dist/js/prod/es5/angular2",
     "automattic/socket.io-client": "github:automattic/socket.io-client@1.3.5"
   }
