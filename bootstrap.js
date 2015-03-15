16144f39c8e372db0a64aefe7a4141582970ffa7
Rob Wormald <rob@jiff.com>
2015-03-14 23:30:34 -0700

    add bootstrapper

---
+++ bootstrap.js
@@ -0,0 +1,27 @@
+/**
+ * ES5 startup script. Loads main ES6 module and runs it.
+ */
+(function(){
+
+  //execute run method on main module
+  function bootstrap(mainModule){
+    return mainModule.run()
+  }
+
+  function importError(err){
+    console.error('Error while importing:',err);
+  }
+
+  function bootstrapError(err){
+    console.error('Error while bootstrapping:',err);
+  }
+
+  console.info('starting app...');
+
+  //use SystemJS to import main.js 
+  System.import('./main')
+  .catch(importError)
+  .then(bootstrap)
+  .catch(bootstrapError);
+
+})()
