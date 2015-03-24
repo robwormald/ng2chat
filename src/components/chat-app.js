fbc6b937dff331d5198bd671f35de726539b636a
Rob Wormald <robwormald@gmail.com>
2015-03-24 12:20:04 -0700

    Revert 59967bd..fa32e07
    This rolls back to commit 59967bd4271779ab6fdf0ec3f0d07bc76f6e7ee7.

--- src/components/chat-app.js
+++ src/components/chat-app.js
@@ -1,35 +1,28 @@
-import {Component, Template, Foreach} from 'angular2/angular2';
+import {Component, Template} from 'angular2/angular2';
 import {bind} from 'angular2/di';
 
 import {SailsIOClient} from '../services/SailsIOClient';
-import {ChatManager, Channel} from '../services/ChatManager';
 
 const SERVER_URL = 'http://angular-sails-chat.herokuapp.com/';
 
-
-
 @Component({
   selector: 'chat-app',
-  services: [
-    bind(SailsIOClient).toValue(new SailsIOClient(SERVER_URL)),
-    ChatManager]
+  services: [bind(SailsIOClient).toValue(new SailsIOClient(SERVER_URL))]
 })
 @Template({
-  url: 'src/components/chat-app.html',
-  directives: [Foreach]
+  url: 'src/components/chat-app.html'
 })
 export class ChatApp {
 
-  selectedChannel: Channel;
-
-  constructor(chatManager: ChatManager){
+  constructor(chatClient: SailsIOClient){
     this.title = 'AngularChat';
-    this.chatManager = chatManager;
-    this.selectedChannel = {messages:[]};
-    this.chatManager.loadChannels()
-    .then((channels)=> {
-      this.channels = channels;
-      this.selectedChannel = channels[0] || {};
-    });
+    this.chatClient = chatClient;
+    this.chatClient.connect()
+    .then(()=>{
+      return this.chatClient.get('/channel')
+    })
+    .then((res)=> {
+      console.log(res)
+    })
   }
 }
